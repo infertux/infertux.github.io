4e07a2f5e2498da971d8989e79a99b411fbfc845
Infertux <infertux@infertux.com>
2026-03-14 02:44:51 +0000

    Update dependencies

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,4 @@ domain=$(basename "$PWD")
 
 ./build.sh
 
-rsync -avzc --delete-excluded \
-    _site/ "hosting-infertux@hosting.cyberbits.eu:${domain}"
+rsync -avzc --delete-excluded _site/ "hosting-infertux@hosting.cyberbits.eu:${domain}"
